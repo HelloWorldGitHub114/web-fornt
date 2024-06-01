21e78e892e2aeef923123fb4d5587b6bd35e34dd
Type_Moon <14270615+type-moonqwq@user.noreply.gitee.com>
2024-06-01 22:06:41 +0800

    FIN

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-
 axios.defaults.baseURL = 'http://localhost:8081';
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8';
 axios.interceptors.request.use((config) => {
